897f9544e405e1c8ada628c485746743e2c6722e
João Pedro T <joaopedrot1912@gmail.com>
2023-01-11 11:42:37 -0300

    update 11/01

---
+++ exercicio proposto 3/imc.js
@@ -0,0 +1,37 @@
+(function() {
+    imc()
+    function imc() {
+        let peso = 0
+        let altura = 0 /* Dados Aqui */ 
+        let imc = 0
+        if(peso === 0 || altura == 0) {
+            throw Error("Dados Incompletos")
+        }
+        if(typeof peso !== "number" || typeof altura !== "number") {
+            throw Error("Only Numbers")
+        } else {
+            imc = peso / (altura**2)
+            console.log(TabImc(imc))
+        }
+    }
+
+    function TabImc(IMC) {
+        if (IMC >= 16 && IMC <= 16.9) {
+            return "Muito Abaixo do Peso"
+        } else if (IMC >=17 && IMC <= 18.4) {
+            return "Abaixo do Peso"
+        } else if(IMC >= 18.5 && IMC <= 24.9) {
+            return "Peso Normal"
+        } else if(IMC >= 24.9 && IMC <= 29.9) {
+            return "Acima do Peso"
+        } else if(IMC >= 30 && IMC <= 34.9) {
+            return "Obesidade Grau 1"
+        } else if(IMC >= 35 && IMC <= 40) {
+            return "Obesidade Grau 2"
+        } else if(IMC >= 40 && IMC <= 90) {
+            return "Obesidade Grau 3"
+        } else {
+            throw Error("Dados Irreais")
+        }
+    }
+})()
