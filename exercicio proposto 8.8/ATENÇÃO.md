428d0bd183f9b662d6eed9e7432e8c50784915eb
João Pedro T <joaopedrot1912@gmail.com>
2023-01-20 11:35:05 -0300

    update 20/01

---
+++ exercicio proposto 8.8/ATENÇÃO.md
@@ -0,0 +1,3 @@
+***
+o html e css desse exercicio não foram feitos por mim e sim copiados para fins não comerciais.
+***
---
+++ exercicio proposto 8.8/script.js
@@ -0,0 +1,53 @@
+const alunos = [
+                { nome: "Daniel", notas: [10, 3, 7.5, 3] },
+                { nome: "Maria", notas: [10, 9, 3, 9.5] },
+                { nome: "João", notas: [10, 4.5, 1, 3.5] },
+                { nome: "Joana", notas: [1, 3, 9, 1.5] },
+                { nome: "José", notas: [10, 4.5, 7, 3] },
+                { nome: "Arnaldo", notas: [10, 4.5, 7, 3] },
+                { nome: "Lucas", notas: [4.5, 9, 8, 3] },
+                { nome: "Luana", notas: [3, 7, 9, 3] },
+                { nome: "Beatriz", notas: [10, 4, 7, 9] },
+                { nome: "Sergio", notas: [4.5, 9.5, 10, 2] }
+];
+
+(function(){
+
+
+    function avarage(notas) {
+        let soma = 0
+        for(let nota of notas) {
+            soma += nota
+        }
+        return soma / 4
+    }
+
+    alunos.forEach(aluno => {
+        aluno.media = avarage(aluno.notas).toFixed(1)
+    })
+
+    const html = alunos.map(aluno => `
+        <tr>
+            <td>${aluno.nome}</td>
+            <td>${aluno.notas[0]}</td>
+            <td>${aluno.notas[1]}</td>
+            <td>${aluno.notas[2]}</td>
+            <td>${aluno.notas[3]}</td>
+            <td>${aluno.media}</td>
+        </tr>
+    `).join("")
+
+    document.querySelector('tbody').innerHTML = html
+
+})()
+
+/*
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+*/ 
