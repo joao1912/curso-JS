fa34ecd1ead1311ecc84529a3f9a9d889ccf7eb7
João Pedro T <joaopedrot1912@gmail.com>
2023-01-27 15:42:25 -0300

    changes

--- teste.js
+++ teste.js
@@ -1,23 +1,8 @@
-let title = document.createElement("h1")
-let atributo = document.createAttribute("class")
-atributo.value = "azul"
-
-title.setAttributeNode(atributo)
-title.textContent = "isso é um titulo"
-
-let parag = document.querySelector("p")
-
-let conteiner = document.getElementById("fundo")
-
-let title2 = title.cloneNode(true)
-
-console.log(title2)
-
-conteiner.prepend(title2)
-conteiner.append(title)
-
-let elemento_novo = document.createElement("p")
-
-elemento_novo.textContent = "isso é um lindo paragrafo de teste feito por joao pedro silva teixeira aprendendo programação"
-
-conteiner.append(elemento_novo)
+(function(){
+    let conteiner = document.querySelector("#conteiner")
+    conteiner.addEventListener("click",function(e){
+        if(e.target.nodeName === "BUTTON") {
+            alert("mas bah guri")
+        }
+    })
+})()
