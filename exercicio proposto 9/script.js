fa8ea3041eb17ecc61d44191052ddeca568100fa
João Pedro T <joaopedrot1912@gmail.com>
2023-01-27 11:07:19 -0300

    update 27/01

--- exercicio proposto 9/script.js
+++ exercicio proposto 9/script.js
@@ -1,10 +1,4 @@
 
-//Mova a última li de orignal para alvo (mas antes da primeira li). 
-//Em outras palavras, a última li da original deve virar a primeira li de alvo
-
-//altere o texto da primeira li de .alvo para "item 0"
-
-//clone a ul de .original e a inclua na primeira li de .alvo
 
 //remova a ultima li (item2) da lista inserida na etapa anterior
 
@@ -13,13 +7,16 @@
 ;(function(){
     let elemento_lista = document.createElement("li")
     elemento_lista.textContent = "item 2"
-    document.querySelector(".alvo").firstElementChild.nextElementSibling.after(elemento_lista)
+    let lista = document.querySelector(".alvo").firstElementChild.nextElementSibling
+    
+    lista.insertAdjacentElement("afterbegin", document.querySelector(".original").firstElementChild.nextElementSibling.lastElementChild)
+    document.querySelector(".alvo").firstElementChild.nextElementSibling.firstElementChild.textContent = "item 0"
+    let clone_ul = document.querySelector(".original").firstElementChild.nextElementSibling.cloneNode(true)
+    console.log(clone_ul)
+    lista.insertAdjacentElement("beforeend",clone_ul)
+    lista.lastElementChild.lastElementChild.remove()
+    lista.lastElementChild.lastElementChild.textContent = "item 2"
 
-    let li_original = document.querySelector(".original").firstElementChild.nextElementSibling.lastElementChild
-    let li_alvo = document.querySelector(".alvo").firstElementChild.nextElementSibling.firstElementChild
-    let li_clone = li_original.cloneNode(true)
-    li_alvo.remove()
-    document.querySelector(".alvo").firstElementChild.nextElementSibling.insertAdjacentElement("beforebegin",li_clone)
-
-    console.log(document.querySelector("ul.lista"))
+    document.querySelector(".original").firstElementChild.nextElementSibling.firstElementChild.remove()
+    document.querySelector(".original").firstElementChild.nextElementSibling.firstElementChild.remove()
 })()
