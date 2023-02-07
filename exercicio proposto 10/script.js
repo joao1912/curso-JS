5e9fefc10040370204870f5b7313bfa90307eb6d
joão <joaopedrot1912@gmail.com>
2023-02-07 11:20:42 -0300

    update 07/02

--- exercicio proposto 10/script.js
+++ exercicio proposto 10/script.js
@@ -1,8 +1,39 @@
 (function(){
-    const arr = [1, 3, 5, "opa", true]
-    const arr_1 = arr.filter(function( el ){
-        return typeof el === "number"
+    'use strict';
+
+    const botaoSubmit = document.getElementById("botaoEnviar")
+    const checkboxPreSubmit = document.getElementById("validacao")
+
+    checkboxPreSubmit.addEventListener("click",function(){
+        if(this.checked) {
+            botaoSubmit.disabled = false
+        } else {
+            botaoSubmit.disabled = true
+        }
     })
-    console.log(arr_1)
+
+    const txt = document.getElementById("title")
+
+    botaoSubmit.addEventListener("click", function(event) {
+        if(!txt.value) {
+            alert("Preencha os dados do formulario")
+            event.preventDefault()
+            checkboxPreSubmit.checked = false
+            txt.focus()
+        }
+    })
+
+    const txtDescricao = document.getElementById("txtarea")
+    const contador = document.getElementById("contador")
+    const txtContador = contador.firstElementChild
+    const caracMaxima = txtDescricao.maxLength
+    
+    contador.removeAttribute("style")
+
+    txtDescricao.addEventListener("input",function() {
+        let letrasDigitadas = this.value.length
+        let letrasSubtraidas = caracMaxima - Number(letrasDigitadas)
+        txtContador.textContent = letrasSubtraidas
+    })
 
 })()
