5ba7186a7cca35cf1f369652493e5d0e45db6245
joão <joaopedrot1912@gmail.com>
2023-02-26 19:13:21 -0300

    pequenas alterações

--- objetos/objeto.js
+++ objetos/objeto.js
@@ -1,2 +1,12 @@
-const teste = new Date()
-console.log(teste.getFullYear())
+function Task(name,complet) {
+    this.name = name
+    this.completed = complet
+    this.upDateAt = new Date()
+}
+
+let task1 = new Task("task nova", false)
+
+let task2 = new Task("task mais que nova", true)
+
+task2.upDateAt =  new Date().getFullYear()
+console.log(task2)
