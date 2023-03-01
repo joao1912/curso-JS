36ace975060df613613f0694ae6c0eba17fe290f
joão <joaopedrot1912@gmail.com>
2023-03-01 11:56:50 -0300

    novo exercicio completo e novas alterações nos testes de objeto

--- objetos/objeto.js
+++ objetos/objeto.js
@@ -1,4 +1,7 @@
 function Task(name,complet) {
+    'use strict'
+    if (this === undefined) return
+
     this.name = name
     this.completed = complet
     this.upDateAt = new Date()
@@ -8,5 +11,6 @@ let task1 = new Task("task nova", false)
 
 let task2 = new Task("task mais que nova", true)
 
-task2.upDateAt =  new Date().getFullYear()
-console.log(task2)
+
+
+
